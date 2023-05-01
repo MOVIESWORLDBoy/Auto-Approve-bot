dd467cbd4e98a52cccc1a324603cb34e96f5b4d1
MOVIESWORLDBoy <117733454+MOVIESWORLDBoy@users.noreply.github.com>
2023-05-01 15:19:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**👋 HELLO {}!\n\nYOUR REQUEST TO JOIN {} HAS BEEN APPROVED ☑️. \n\nThanks For Joining \n /start to know more.**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**👋 HELLO {}!\n\nYOUR REQUEST TO JOIN {} HAS BEEN APPROVED ☑️. \n\nThanks For Joining 😎 \n\n Send /start to know more.**".format(m.from_user.mention, m.chat.title))
 
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
